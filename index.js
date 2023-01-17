3aba1a9518f2d2152b528ec27d67c7181b24041a
abcushing <abcushing789@gmail.com>
2023-01-17 12:33:30 -0700

    5th-commit
    5th commit added more to the index files and set up dist json file

--- index.js
+++ index.js
@@ -1,34 +1,31 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-inquirer
-  .prompt([
-    {
-      type: "checkbox",
-      message: "What is the employee role?",
-      name: "role",
-      choices: ["Manager", "Engineer", "Intern", "Employee"],
-    },
-  ])
-  .then((data) => {
-    console.log("data role= ", data);
-    if (data.role[0] === "Manager") {
-      getManagerData();
-    } else if (data.role[0] === "Engineer") {
-      getEngineerData();
-    } else if (data.role[0] === "Intern") {
-      getInternData();
-    } else if (data.role[0] === "Employee") {
-      getEmployeeData();
-    }
+function addEmployee() {
+  inquirer
+    .prompt([
+      {
+        type: "checkbox",
+        message: "What is the employee role?",
+        name: "role",
+        choices: ["Manager", "Engineer", "Intern", "Employee"],
+      },
+    ])
+    .then((data) => {
+      console.log("data role= ", data);
+      if (data.role[0] === "Manager") {
+        getManagerData();
+      } else if (data.role[0] === "Engineer") {
+        getEngineerData();
+      } else if (data.role[0] === "Intern") {
+        getInternData();
+      } else if (data.role[0] === "Employee") {
+        getEmployeeData();
+      }
 
-    nextEmployee();
-    // const filename = `${data.name.toLowerCase().split(" ").join("")}.json`;
-
-    // fs.writeFile(filename, JSON.stringify(data, null, "\t"), (err) =>
-    //   err ? console.log(err) : console.log("Success!")
-    // );
-  });
+      // const filename = `${data.name.toLowerCase().split(" ").join("")}.json`;
+    });
+}
 
 function getManagerData() {
   console.log("before employee ");
@@ -59,6 +56,12 @@ function getManagerData() {
       console.log("data.role= ", data.role);
       data.role = "Manager";
       console.log(data);
+      fs.appendFile(
+        "./dist/employees.json",
+        JSON.stringify(data, null, "\t"),
+        (err) => (err ? console.log(err) : console.log("Success!"))
+      );
+      nextEmployee();
     });
 }
 
@@ -89,6 +92,12 @@ function getEngineerData() {
     .then((data) => {
       data.role = "Engineer";
       console.log(data);
+      fs.appendFile(
+        "./dist/employees.json",
+        JSON.stringify(data, null, "\t"),
+        (err) => (err ? console.log(err) : console.log("Success!"))
+      );
+      nextEmployee();
     });
 }
 
@@ -119,36 +128,12 @@ function getInternData() {
     .then((data) => {
       data.role = "Intern";
       console.log(data);
-    });
-}
-
-function getManagerData() {
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "Employee name",
-        message: "What is the employee name?",
-      },
-      {
-        type: "input",
-        name: "Employee email",
-        message: "What is the employee email?",
-      },
-      {
-        type: "input",
-        name: "Employee ID",
-        message: "What is the employee ID?",
-      },
-      {
-        type: "input",
-        name: "Employee Office Number",
-        message: "What is the employee Office Number?",
-      },
-    ])
-    .then((data) => {
-      data.role = "Manager";
-      console.log(data);
+      fs.appendFile(
+        "./dist/employees.json",
+        JSON.stringify(data, null, "\t"),
+        (err) => (err ? console.log(err) : console.log("Success!"))
+      );
+      nextEmployee();
     });
 }
 
@@ -174,6 +159,12 @@ function getEmployeeData() {
     .then((data) => {
       data.role = "Employee";
       console.log(data);
+      fs.appendFile(
+        "./dist/employees.json",
+        JSON.stringify(data, null, "\t"),
+        (err) => (err ? console.log(err) : console.log("Success!"))
+      );
+      nextEmployee();
     });
 }
 
@@ -189,5 +180,12 @@ function nextEmployee() {
     ])
     .then((data) => {
       console.log(data);
+      if (data.continue[0] === "y") {
+        addEmployee();
+      } else {
+        console.log("added employees");
+      }
     });
 }
+
+addEmployee();
